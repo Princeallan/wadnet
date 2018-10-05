f83aae36ae5bcc1181586c3aaa7caccd0fa742ba
Allan Kirui <akirui@cytonn.com>
2018-10-05 06:31:41 +0300

    Footer modified

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,12 +1,40 @@
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
 
-<div class="footer-section" style="display: flex;justify-content: center;align-items: center;margin: 5em;">
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 item">
-            <p>
-                <a href="#">fb</a>
-                <a href="#">fb</a>
-            </p>
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-12 py-5">
+                <div class="mb-5 flex-center">
+
+                    <a class="fb-ic">
+                        <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="tw-ic">
+                        <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="gplus-ic">
+                        <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="li-ic">
+                        <i class="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="ins-ic">
+                        <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+                    </a>
+                    <a class="pin-ic">
+                        <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
+                    </a>
+                </div>
+            </div>
+
         </div>
+
     </div>
 
-</div>
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+        <a href="http://wadnetwork.com"> MDBootstrap.com</a>
+    </div>
+
+</footer>
