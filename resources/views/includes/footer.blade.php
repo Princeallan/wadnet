3ed55b1f2ba4950d4ec7f445060a47deb8aa0c4a
Allan Kirui <akirui@cytonn.com>
2018-10-05 06:42:37 +0300

    Footer final Changes

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,41 +1,48 @@
 <!-- Footer -->
-<footer class="page-footer font-small special-color-dark pt-4">
+<footer class="page-footer font-small stylish-color-dark pt-4">
 
-    <!-- Footer Elements -->
-    <div class="container">
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-2">
+        <li class="list-inline-item">
+            <h5 class="mb-1">Join Our Communnity</h5>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" class="btn btn-danger btn-rounded">Join Now!</a>
+        </li>
+    </ul>
+    <!-- Call to action -->
 
-        <!-- Social buttons -->
-        <ul class="list-unstyled list-inline text-center">
-            <li class="list-inline-item">
-                <a class="btn-floating btn-fb mx-1" href="http://wadnetwork.com">
-                    <i class="fa fa-facebook"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-tw mx-1" href="http://wadnetwork.com">
-                    <i class="fa fa-twitter"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-gplus mx-1" href="http://wadnetwork.com">
-                    <i class="fa fa-google-plus"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-li mx-1" href="http://wadnetwork.com">
-                    <i class="fa fa-linkedin"> </i>
-                </a>
-            </li>
-            <li class="list-inline-item">
-                <a class="btn-floating btn-dribbble mx-1" href="http://wadnetwork.com">
-                    <i class="fa fa-dribbble"> </i>
-                </a>
-            </li>
-        </ul>
-        <!-- Social buttons -->
+    <hr>
 
-    </div>
-    <!-- Footer Elements -->
+    <!-- Social buttons -->
+    <ul class="list-unstyled list-inline text-center">
+        <li class="list-inline-item">
+            <a class="btn-floating btn-fb mx-1" href="http://wadnetwork.com">
+                <i class="fa fa-facebook"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-tw mx-1" href="http://wadnetwork.com">
+                <i class="fa fa-twitter"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-gplus mx-1" href="http://wadnetwork.com">
+                <i class="fa fa-google-plus"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-li mx-1" href="http://wadnetwork.com">
+                <i class="fa fa-linkedin"> </i>
+            </a>
+        </li>
+        <li class="list-inline-item">
+            <a class="btn-floating btn-dribbble mx-1" href="http://wadnetwork.com">
+                <i class="fa fa-dribbble"> </i>
+            </a>
+        </li>
+    </ul>
+    <!-- Social buttons -->
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2018 Copyright:
